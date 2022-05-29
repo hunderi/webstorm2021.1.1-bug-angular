8ca56382ac2d04d5fb6b00ce62a021c425b3706d
hunderi <55706662+hunderi@users.noreply.github.com>
2022-05-29 22:16:14 +0200

    added more modules and a slider to test more angular material references

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,25 +4,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { DcTextInputComponent } from './extended-components/dc-text-input/dc-text-input.component';
-
 import { MatInputModule } from '@angular/material/input';
-
 import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
-
+import { MatSliderModule } from '@angular/material/slider';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 @NgModule({
   declarations: [
     AppComponent,
     DcTextInputComponent
   ],
   imports: [
+    ReactiveFormsModule,
+    FormsModule,
+    CommonModule,
     BrowserModule,
     BrowserAnimationsModule,
     MatInputModule,
+    MatSliderModule,
     MatFormFieldModule,
-    FormsModule
-
     MatIconModule
   ],
   providers: [],
