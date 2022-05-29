7e1b7942ccf0c0c8ea82225996efc1ec42e275c1
hunderi <55706662+hunderi@users.noreply.github.com>
2022-05-29 20:58:59 +0200

    added missing matIcon module

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,7 @@ import { DcTextInputComponent } from './extended-components/dc-text-input/dc-tex
 
 import { MatInputModule } from '@angular/material/input';
 
+import {MatIconModule} from '@angular/material/icon';
 import {MatFormFieldModule} from '@angular/material/form-field';
 
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
@@ -22,6 +23,7 @@ import { FormsModule, ReactiveFormsModule } from '@angular/forms';
     MatFormFieldModule,
     FormsModule
 
+    MatIconModule
   ],
   providers: [],
   bootstrap: [AppComponent]
